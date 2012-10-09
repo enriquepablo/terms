54c755752a889f7257b5fc62a1e7f1e784ef83e2
Enrique Perez Arnaud <enriquepablo@gmail.com>
2012-10-09 23:57:21 +0200

    use distribute, GH-16

--- setup.py
+++ setup.py
@@ -5,35 +5,33 @@ from setuptools import setup
 VERSION = '0.1.0a1'
 
 setup(
-    name = 'terms.core',
+    name = 'Terms',
     version = VERSION,
     author = 'Enrique Pérez Arnaud',
     author_email = 'enriquepablo@gmail.com',
     url = 'http://pypi.python.org/terms.core',
     license = 'GNU GENERAL PUBLIC LICENSE Version 3',
-    description = '',
+    description = 'A smart knowledge store',
     long_description = open('INSTALL.txt').read() + open('README.txt').read(),
 
-    zip_safe = False,
-    packages=['terms', 'terms.core',],
-    namespace_packages=['terms'],
-    include_package_data = True,
-    scripts = ['bin/terms', 'bin/nosetests', 'bin/coverage'],
-
+    packages = ['terms', 'terms.core',],
+    namespace_packages = ['terms'],
+    py_modules = ['distribute_setup'],
     test_suite = 'nose.collector',
+    include_package_data = True,
 
     entry_points = {
         'console_scripts': [
             'terms = terms.core.repl:repl',
-#            'initterms = terms.core.initialize:initterms',
+            'initterms = terms.core.scripts.initterms:init_terms',
         ],
-    }
-    tests_require=[
+    },
+    tests_require = [
         'Nose',
         'coverage',
     ],
-    install_requires=[
-        'psycopg2',
+    install_requires = [
+        'psycopg2 == 2.4.5',
         'sqlalchemy == 0.7.8',
         'ply == 3.4',
     ],
